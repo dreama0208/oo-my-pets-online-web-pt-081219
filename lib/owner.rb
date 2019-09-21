0a1cca567909048c52eb8420b057344ee050349b
imperative-genre-4923 <dreama.lin@yale.edu>
2019-09-21 16:27:19 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -18,6 +18,11 @@ class Owner
     Cat.all.select {|cat| cat.owner == self}
   end
 
+  def dogs
+    Dog.all.select {|dog| dog.owner == self}
+  end
+
+
   def buy_cat(cat_name)
     if cat = Cat.all.detect {|cat| cat.name == cat_name }
       cat.owner = self
