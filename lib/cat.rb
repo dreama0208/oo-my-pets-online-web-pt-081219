9a6d0d19a0295fcdd741645839eb78338b7739df
imperative-genre-4923 <dreama.lin@yale.edu>
2019-09-21 16:13:05 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,7 +1,7 @@
 class Cat
   # code goes here
-  attr_accessor :name, :owner
-  attr_writer :mood
+  attr_reader :name
+  attr_accessor :owner, :mood
   @@all = []
 
   def initialize(name, owner, mood = "nervous")
