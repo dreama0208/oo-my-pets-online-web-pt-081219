a31aafc800901a1d7caa36c285abbe1e89d3f803
imperative-genre-4923 <dreama.lin@yale.edu>
2019-09-21 16:10:51 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,21 @@
 class Cat
   # code goes here
+  attr_accessor :name, :owner
+  attr_writer :mood
+  @@all = []
+
+  def initialize(name, owner, mood = "nervous")
+    @name = name
+    @owner.name = owner
+    @mood = mood
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def count
+    @@all.length
+  end
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,46 @@
 class Owner
   # code goes here
+  attr_accessor :name
+  attr_writer :species
+  @@all = []
+
+  def initialize(name, species = "human")
+    @name = name
+    @species = species
+    @@all << self
+  end
+
+  def say_species
+    print "I am a human."
+  end
+
+  def cat
+    Cat.all.collect {|cat| cat.owner == self}
+  end
+
+  def buy_cat(cat_name)
+    if cat = Cat.all.detect {|cat| cat.name == cat_name }
+      cat.owner = self
+    end
+  end
+
+  def feed_cats
+    self.cat.each {|cat| cat.mood = "happy"}
+  end
+
+  def sell_pets
+    self.cat.each {|cat| cat.mood = "nervous" && cat.owner = nil }
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.length
+  end
+
+  def reset_all
+    @@all.clear
+  end
 end
