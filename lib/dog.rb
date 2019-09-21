38007bb0d77cbd9efa857650e62e433610f5873b
imperative-genre-4923 <dreama.lin@yale.edu>
2019-09-21 16:22:00 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,22 @@
 class Dog
   # code goes here
+  attr_reader :name
+  attr_accessor :owner, :mood
+  @@all = []
 
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def count
+    @@all.length
+  end
 
 end
